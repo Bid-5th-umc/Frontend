6469ff8530c73e651efbbd5fde20ef56460a98ca
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2024-01-15 18:21:47 +0900

    [Initial Setting] Layout Setting

---
+++ bid-web/src/Components/Layout/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Logo/>
+      Header
+    </div>
+  )
+}
+
+export default Header
---
+++ bid-web/src/Components/Layout/Header/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Logo = () => {
+  return (
+    <div to={'/'} className='logo'>
+      <img src="image/bid-logo.png" alt="bid"></img>
+    </div>
+  )
+}
+
+export default Logo
