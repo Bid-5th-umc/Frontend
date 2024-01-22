6711fc9e135b867b7caa7c41187d62494ea3dd29
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2024-01-23 01:32:30 +0900

    [feature2] Shop Main 화면 Link 구현

--- bid-web/src/App.js
+++ bid-web/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './Pages/MainPage';
+import ShopMainPage from './Pages/ShopMainPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/sell" element={<ShopMainPage />} />
     </Routes>
   );
 }
