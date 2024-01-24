290c2e40f8139fff846450df3dfafa6cdfafb32b
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2024-01-25 03:14:09 +0900

    [feature3] 카테고리 hover 추가 미완성

--- bid-web/src/Components/Common/CategoryBar.js
+++ bid-web/src/Components/Common/CategoryBar.js
@@ -4,25 +4,16 @@ import CategoryCom from './CategoryCom';
 
 const CategoryBar = () => {
   const categoryData = [
-    { id: "/", name: "DESIGNERS" },
-    { id: "/", name: "MENSWEAR" },
-    { id: "/", name: "WOMENSWEAR" }
+    { name: "DESIGNERS" },
+    { name: "MENSWEAR" },
+    { name: "WOMENSWEAR" }
   ];
 
   return (
     <div className='categoryBar'>
-      <CategoryCom
-        PageUrl={categoryData[0].id}
-        CategoryName={categoryData[0].name}
-      />
-      <CategoryCom
-        PageUrl={categoryData[1].id}
-        CategoryName={categoryData[1].name}
-      />
-      <CategoryCom
-        PageUrl={categoryData[2].id}
-        CategoryName={categoryData[2].name}
-      />
+      <CategoryCom CategoryName={categoryData[0].name}/>
+      <CategoryCom CategoryName={categoryData[1].name}/>
+      <CategoryCom CategoryName={categoryData[2].name}/>
     </div>
   )
 }
--- bid-web/src/Components/Common/CategoryCom.js
+++ bid-web/src/Components/Common/CategoryCom.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
+import HoverCard from './HoverCard';
 
-const CategoryCom = ({ PageUrl, CategoryName }) => {
+const CategoryCom = ({ CategoryName }) => {
+
+  const [isHovered, setHovered] = useState(false);
+
   return (
     <div
-      to={PageUrl}
-      className='category'     
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className={`category ${isHovered ? 'hovered' : ''}`}
     >
       <span>
         {CategoryName}
       </span>
       <img src='/image/MoreIcon.png' alt='more' className='categoryImg'></img>
+      {isHovered && <HoverCard/>}
     </div>
   )
 }
