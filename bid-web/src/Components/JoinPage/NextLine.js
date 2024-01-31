45926395c144fcfa902d676262bc3c75e580af6e
unknown <crystal5k123@gmail.com>
2024-02-01 03:09:39 +0900

    [feature 9, 10] 9 일부, 10 전체 퍼블리싱

--- bid-web/src/Components/JoinPage/NextLine.js
+++ bid-web/src/Components/JoinPage/NextLine.js
@@ -4,7 +4,7 @@ import './Join.css';
 const NextLine = () => {
   return (
 
-    <div class="NextLine">
+    <div className="NextLine">
         <img src="image/join-line2.png" className="join-line2" alt="join-line2"/>
         <div className="Information">회원정보 입력</div>
     </div>
