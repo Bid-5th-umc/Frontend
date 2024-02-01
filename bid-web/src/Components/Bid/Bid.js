0110ca222a3f8915db69786b02b86eb363702df3
Seo-Jooyoung <136756125+Seo-Jooyoung@users.noreply.github.com>
2024-02-02 01:57:26 +0900

    [feature6] 경매 페이지 카테고리 바 추가 및 margin 조정

--- bid-web/src/Components/Bid/Bid.js
+++ bid-web/src/Components/Bid/Bid.js
@@ -2,17 +2,21 @@ import React from 'react'
 import './Bid.css'
 import BidInfo from './BidInfo';
 import BidDetail from './BidDetail';
+import CategoryBar from '../Common/CategoryBar';
 
 const Bid = ({ children }) => {
   return (
-    <div className='bidPage'>
-      <div className='bidPage-containerTop'>
-        <div className='bidPage-container'>
-          <BidInfo />
-          <BidDetail />
-        </div>
-      </div> 
-    </div>
+    <>
+      <CategoryBar/>
+      <div className='bidPage'>
+        <div className='bidPage-containerTop'>
+          <div className='bidPage-container'>
+            <BidInfo />
+            <BidDetail />
+          </div>
+        </div> 
+      </div>
+    </>
   )
 }
 
